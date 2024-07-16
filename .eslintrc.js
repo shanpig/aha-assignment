e5b13f6aa65d8871255911d35969d806b494ed5f
david <davidLiao@hahow.in>
2024-07-16 20:00:35 +0800

    feat: configure eslint airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
-{
-  "extends": [
+module.exports = {
+  extends: [
     "next/core-web-vitals",
-    "airbnb",
-    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "eslint:recommended",
-    "plugin:react/recommended",
     "prettier"
   ],
-  "plugins": ["@typescript-eslint"],
-  "rules": {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
     "no-undef": "off"
   }
-}
+};
