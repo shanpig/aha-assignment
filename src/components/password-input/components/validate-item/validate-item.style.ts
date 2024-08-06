6878d22ae47184a3b8dea8cbb3fc78b71ddcf2fd
david <davidLiao@hahow.in>
2024-08-06 19:37:06 +0800

    feat: styling

---
+++ src/components/password-input/components/validate-item/validate-item.style.ts
@@ -0,0 +1,14 @@
+import { styled } from "@mui/material";
+
+export const StyledItem = styled("div")`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding-block: 8px;
+
+  & > span {
+    font-size: 14px;
+    line-height: 21px;
+    letter-spacing: 0.25px;
+  }
+`;
--- src/components/password-input/components/validate-item/validate-item.tsx
+++ src/components/password-input/components/validate-item/validate-item.tsx
@@ -1,17 +1,18 @@
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
 
+import { StyledItem } from "./validate-item.style";
 import { ValidateItemProps } from "./validate-item.type";
 
 export default function ValidateItem({ isValid, children }: ValidateItemProps) {
   return (
-    <div className="flex items-center gap-2 py-2">
+    <StyledItem>
       {isValid ? (
         <CheckCircleIcon sx={{ color: "primary.light" }} />
       ) : (
         <CheckCircleOutlinedIcon color="disabled" />
       )}
       <span>{children}</span>
-    </div>
+    </StyledItem>
   );
 }
